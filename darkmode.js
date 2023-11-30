a11def753d48d4b3b69ba4b3ec8e255ff0928dc5
cesarNeZe <cesarzaitoun@gmail.com>
2023-11-30 11:34:52 +0200

    improve syle for nav

--- darkmode.js
+++ darkmode.js
@@ -1,10 +1,10 @@
 
 
-document.addEventListener('DOMContentLoaded',()=>{
+document.addEventListener('DOMContentLoaded', () => {
     const root = document.documentElement;
     const toggleBtn = document.getElementById('dark-mode');
-    
-    toggleBtn.addEventListener('click', function(){
+
+    toggleBtn.addEventListener('click', function () {
         root.classList.toggle('dark-mode');
 
     })
